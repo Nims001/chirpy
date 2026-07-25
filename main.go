c46e36507f4adeb007a8ab1bd1190d6d6508326f
Nimesh <nimeshsitoula@gmail.com>
2026-07-25 13:09:08 +0545

    added one line, restarting this course again after a long time

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main(){
 		Addr:           ":8080",
 		Handler:        sermux,
 	}
-	sermux.Handle("/",http.FileServer(http.Dir("./")))
+	sermux.Handle("/app/",http.FileServer(http.Dir("./")))
 
 	err := s.ListenAndServe()
 	if err != nil{
